53d18b568d6ac572d44aaff7690100a7ad1de5cd
Poornima-2011 <poornimasiva20@gmail.com>
2026-07-18 16:37:25 +0530

    Day 2 - HTML pages completed

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
