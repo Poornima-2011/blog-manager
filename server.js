18156ef7771388875159bccc106b791adaa085bb
Poornima-2011 <poornimasiva20@gmail.com>
2026-07-20 14:54:08 +0530

    Completed Day 5 - Created Express server with GET and POST routes

--- server.js
+++ server.js
@@ -2,10 +2,40 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const PORT = 3000;
 
-// Serve static files from public folder
+// Middleware
+app.use(express.json());
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+// Home Route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// GET Route
+app.get("/blogs", (req, res) => {
+    res.json({
+        message: "GET request working successfully!",
+        blogs: []
+    });
+});
+
+// POST Route
+app.post("/blogs", (req, res) => {
+
+    const blog = req.body;
+
+    res.status(201).json({
+        message: "Blog received successfully!",
+        data: blog
+    });
+
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
